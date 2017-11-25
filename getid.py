9dbb29b761f4f49057b8b8bc08d7b96ad523701a
Your Name <you@example.com>
2017-11-25 12:52:03 +0800

    4

--- getid.py
+++ getid.py
@@ -52,4 +52,6 @@ sortlist=['status','location_speed','lremote_addr','httpcode_5m_200','httpcode_5
 'httpcode_1d_302','httpcode_1d_404','httpcode_1d_403','httpcode_1d_500','body_bytes_sent','upstream_response_time','httpcode_total_200',\
 'httpcode_total_302','httpcode_total_404','httpcode_total_403','httpcode_total_500','request_time','location_city','is_good']
 
-print(sortlist)
+# print(sortlist)
+
+print(type(0.))
